27da9a46ce7ac5aecaa221f2ec5d29d3e7434e18
Elaman <elaman@outlook.com>
2019-03-19 14:32:41 +0600

    Randomize position

--- src/components/Pizza/PizzaPreview/PizzaIngredient/PizzaIngredient.js
+++ src/components/Pizza/PizzaPreview/PizzaIngredient/PizzaIngredient.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './PizzaIngredient.module.css';
 
 import tomatoSvg from '../../../../assets/images/tomato.svg';
@@ -9,46 +9,71 @@ import redPepperSvg from '../../../../assets/images/redPepper.svg';
 import yellowPepperSvg from '../../../../assets/images/yellowPepper.svg';
 import unknownSvg from '../../../../assets/images/unknown.svg';
 
-function PizzaIngredient(props) {
-  let ingredient = null;
-
-  switch(props.type) {
-    case 'tomato':
-      ingredient = { width: 35, height: 35, src: tomatoSvg };
-      break;
-    case 'salami':
-      ingredient = { width: 30, height: 30, src: salamiSvg };
-      break;
-    case 'blackOlive':
-      ingredient = { width: 10, height: 10, src: blackOliveSvg
-      };
-      break;
-    case 'greenOlive':
-      ingredient = { width: 10, height: 10, src: greenOliveSvg };
-      break;
-    case 'redPepper':
-      ingredient = { width: 20, height: 20, src: redPepperSvg };
-      break;
-    case 'yellowPepper':
-      ingredient = { width: 40, height: 40, src: yellowPepperSvg };
-      break;
-    default:
-      ingredient = { width: 10, height: 10, src: unknownSvg };    
-      break;
+class PizzaIngredient extends Component {
+  
+  shouldComponentUpdate() {
+    return false;
   }
 
-  if (props.randomPosition) {
-    ingredient.style = {
-      position: 'absolute',
-      top: Math.round(Math.random() * 270) + 'px',
-      left: Math.round(Math.random() * 270) + 'px'
+  getPosition = (ir) => {
+    const pd = 270;
+    const pr = pd / 2;
+
+    const ix = Math.round(Math.random() * pd);
+    const iy = Math.round(Math.random() * pd);
+
+    const distance = Math.sqrt(
+      Math.pow(ix - pr, 2) + Math.pow(iy - pr, 2)
+    ) + ir;
+
+    return distance < pr
+      ? { x: ix - ir, y: iy - ir }
+      : this.getPosition(ir);
+  }
+
+  render() {
+    let ingredient = null;
+
+    switch(this.props.type) {
+      case 'tomato':
+        ingredient = { width: 35, height: 35, src: tomatoSvg };
+        break;
+      case 'salami':
+        ingredient = { width: 30, height: 30, src: salamiSvg };
+        break;
+      case 'blackOlive':
+        ingredient = { width: 10, height: 10, src: blackOliveSvg
+        };
+        break;
+      case 'greenOlive':
+        ingredient = { width: 10, height: 10, src: greenOliveSvg };
+        break;
+      case 'redPepper':
+        ingredient = { width: 20, height: 20, src: redPepperSvg };
+        break;
+      case 'yellowPepper':
+        ingredient = { width: 40, height: 40, src: yellowPepperSvg };
+        break;
+      default:
+        ingredient = { width: 10, height: 10, src: unknownSvg };    
+        break;
     }
+  
+    if (this.props.randomPosition) {
+      const position = this.getPosition(ingredient.width / 2);
+  
+      ingredient.style = {
+        position: 'absolute',
+        top: position.y + 'px',
+        left: position.x + 'px'
+      }
+    }
+  
+    return <img
+      {...ingredient}
+      className={classes.PizzaIngredient}
+      alt={this.props.type} />;
   }
-
-  return <img
-    {...ingredient}
-    className={classes.PizzaIngredient}
-    alt={props.type} />;
 }
 
 export default PizzaIngredient;
