7a309f424406859d1653c299139bff2a07503eb2
Elaman Imashov <elaman@outlook.com>
2019-04-05 15:23:36 +0600

    Fill and stylize CheckoutForm component

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -4,7 +4,14 @@ import classes from './CheckoutForm.module.css';
 function CheckoutForm(props) {
   return (
     <div className={classes.CheckoutForm}>
-      Form
+      <h3>Checkout</h3>
+
+      <label>Name: <input type="text" placeholder="e.g. Bakyt" /></label>
+      <label>Phone: <input type="text" placeholder="e.g. 0 555 123 456" /></label>
+      <label>Address: <input type="text" placeholder="e.g. 150 Karasaeva str." /></label>
+      
+      <button>Submit</button>
+      <button>Cancel</button>
     </div>
   );
 }
